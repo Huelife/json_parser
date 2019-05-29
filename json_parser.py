9886e116223e6b15707f719818feaa9b7a057067
Eric Meas <49707840+Huelife@users.noreply.github.com>
2019-05-29 14:14:24 -0700

    Update json_parser.py
    added comment for with open

--- json_parser.py
+++ json_parser.py
@@ -2,6 +2,7 @@
 
 import json
 
+#user inputs .json file then it is loaded and displayed on the console
 with open(input('.json file name: '), 'r') as jsonfile:
   data_dict = json.load(jsonfile)
   print(json.dumps(data_dict,indent=2,sort_keys=True))
